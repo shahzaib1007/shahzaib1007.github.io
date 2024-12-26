0dbfdb818d72cab98251f3bf2e2c6bd60e1d783d
shahzaib1007 <khan.shahzaib1007@gmail.com>
2024-12-26 00:11:10 -0800

    Updated the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,22 +105,30 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+        <div>
+          Built by{''}
+          <a href="https://github.com/shahzaib1007/" target="_blank" rel="noopener noreferrer">
+            Shahzaib Khan
+          </a>
+          {''}
+          &amp; Designed by{''}
+          <a href="https://brittanychiang.com/" target="_blank" rel="noopener noreferrer">
+            Brittany Chiang
+          </a>
+        </div>
 
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
-        </a>
+        {githubInfo.stars && githubInfo.forks && (
+          <div className="github-stats">
+            <span>
+              <Icon name="Star" />
+              <span>{githubInfo.stars.toLocaleString()}</span>
+            </span>
+            <span>
+              <Icon name="Fork" />
+              <span>{githubInfo.forks.toLocaleString()}</span>
+            </span>
+          </div>
+        )}
       </StyledCredit>
     </StyledFooter>
   );
