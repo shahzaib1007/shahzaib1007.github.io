02bdd513393a56928ca0f5f1c4a2d7244d06ea21
shahzaib1007 <khan.shahzaib1007@gmail.com>
2025-01-01 13:25:01 -0800

    Added brief description and interests

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,14 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'Water Resources Management',
+    'Irrigation',
+    'Evapotranspiration',
+    'Climate',
+    'Hydrological Modelling',
+    'Networks',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,33 +142,12 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Shahzaib and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              I study Earth's surface water resources and explore sustainable, and efficient ways in
+              applications of these water resources. My work leverages satellite remote sensing and
+              modeling to develop practical tools that bridge the gap between research and
+              real-world applications!
             </p>
-
-            <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p>
-
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            <p>Here are a few topics that interests me:</p>
           </div>
 
           <ul className="skills-list">
