93a697a6b96921a56d41299ce1cc16f110ef47b6
shahzaib1007 <khan.shahzaib1007@gmail.com>
2025-01-02 21:37:09 -0800

    Added [] to email id

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,10 @@ module.exports = {
   siteMetadata: {
     title: 'Shahzaib Khan',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Shahzaib Khan is a hydrologist who specializes in managing water resources through satellite remote Sensing.',
+    siteUrl: 'https://shahzaib1007.github.io/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,13 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Shahzaib Khan',
+        short_name: 'Shahzaib Khan',
         start_url: '/',
-        background_color: config.colors.darkNavy,
-        theme_color: config.colors.navy,
+        background_color: config.colors.black,
+        theme_color: config.colors.black,
         display: 'minimal-ui',
-        icon: 'src/images/logo2.png',
+        icon: 'src/images/logo.png',
       },
     },
     `gatsby-plugin-offline`,
