d4dbc6d797626a88334ec4d8655373cc663ed585
shahzaib1007 <khan.shahzaib1007@gmail.com>
2025-01-04 15:33:53 -0800

    Updated contact para

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,9 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        As a researcher, I value collaboration and the exchange of knowledge. My inbox is always
+        open for questions, insights, or just a friendly hello. I’ll do my best to respond as
+        quickly as possible!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
