7271f58153768924b32cad57ae4617998b588599
shahzaib1007 <khan.shahzaib1007@gmail.com>
2025-05-18 16:49:43 -0700

    added insta and twitter username

--- src/config.js
+++ src/config.js
@@ -8,11 +8,11 @@ module.exports = {
     },
     // {
     //   name: 'Instagram',
-    //   url: 'https://www.instagram.com/bchiang7',
+    //   url: 'https://www.instagram.com/shahzaib1007/',
     // },
     // {
     //   name: 'Twitter',
-    //   url: 'https://twitter.com/bchiang7',
+    //   url: 'https://twitter.com/shahzaib1007',
     // },
     {
       name: 'Linkedin',
