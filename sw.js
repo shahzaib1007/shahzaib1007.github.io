9885f59780aba7a49c90c824cd4f1122bd7d5e84
shahzaib1007 <khan.shahzaib1007@gmail.com>
2026-06-15 23:47:13 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4221e5967f6045ab7640.js"
+    "url": "webpack-runtime-59ea50740ccf986e0ccb.js"
   },
   {
     "url": "framework-2e90fe8556dd7b30542d.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "af609bff5cc03a2c568db2e092e8e003"
+    "revision": "cb809d9a3c5877e051ed1294470f5419"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-a270c449457cc37ee291.js"
